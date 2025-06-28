0a65e0182c5f9d783399d3015abba9d880ded7e3
andre <andre.cainda.524@outlook.pt>
2025-06-28 21:44:25 +0100

    Init web project

---
+++ Salescet95.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Salescet95.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
